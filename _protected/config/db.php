ab4eb8349981bebe56f93fbfd1d5270dc9f376f3
djn21 <dejandjekanovic@outlook.com>
2016-06-03 20:43:48 +0200

    01_Instalirana_tema

---
+++ _protected/config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=dejand',
+    'username' => 'dejand',
+    'password' => '35308',
+    'charset' => 'utf8',
+    'tablePrefix' => 'proman_',
+];
